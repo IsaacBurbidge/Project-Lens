060a33b2b15176114cf92d31d9a4c77b2745592c
IsaacBurbidge <114247599+IsaacBurbidge@users.noreply.github.com>
2023-11-03 15:57:55 +0000

    Created Reveal Lens Scripts

---
+++ Assets/Isaacs Work/Scripts/Lens.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lens : MonoBehaviour
+{
+	enum LensList
+	{
+		NONE = -1,
+		REVEAL,
+		REVERSE,
+		REALITY
+
+
+	}
+
+	public void ActivateLens()
+	{
+
+	}
+}
