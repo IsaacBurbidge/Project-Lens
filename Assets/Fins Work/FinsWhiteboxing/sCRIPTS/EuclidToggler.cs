716253d9f5420c2dece9ee62fc586e8721ca39ac
JoshEU <joshjbrown706@gmail.com>
2023-11-29 19:32:09 +0000

    Reality Tutorial Complete

--- Assets/Fins Work/FinsWhiteboxing/sCRIPTS/EuclidToggler.cs
+++ Assets/Fins Work/FinsWhiteboxing/sCRIPTS/EuclidToggler.cs
@@ -7,9 +7,30 @@ public class EuclidToggler : Eventy
     public GameObject roomA;
     public GameObject roomB;
 
+    [SerializeField]
+    private SwitchLens swapLensScript;
+
     public void EuclidToggle()
     {
-        roomA.SetActive(!roomA.activeSelf);
-        roomB.SetActive(!roomB.activeSelf);
+        switch (swapLensScript.CurrentLens) {
+            case Lens.LensList.NONE:
+                roomA.SetActive(true);
+                roomB.SetActive(false);
+                break;
+            case Lens.LensList.REVEAL:
+                roomA.SetActive(true);
+                roomB.SetActive(false);
+                break;
+            case Lens.LensList.REVERSE:
+                roomA.SetActive(true);
+                roomB.SetActive(false);
+                break;
+            case Lens.LensList.REALITY:
+                roomA.SetActive(false);
+                roomB.SetActive(true);
+                break;
+        }
+        //roomA.SetActive(!roomA.activeSelf);
+        //roomB.SetActive(!roomB.activeSelf);
     }
 }
