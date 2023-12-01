5a04f5795b18f85fb3bab64b9dc30c2f574e45a8
Finlay John <findominator7@gmail.com>
2023-12-01 12:38:39 +0000

    Scene transitions made + implement
    made and implemented scene transitions on the main menu exclusively, more to come

--- Assets/Fins Work/FinsWhiteboxing/sCRIPTS/LoaderButton.cs
+++ Assets/Fins Work/FinsWhiteboxing/sCRIPTS/LoaderButton.cs
@@ -4,12 +4,29 @@ using UnityEngine;
 
 public class LoaderButton : MonoBehaviour
 {
+    [Header("Level Loading")]
     [SerializeField] string levelName;
     [SerializeField] LevelSelector levelSelector;
+    [Header("Animation")]
+    [SerializeField] Animator levelAnimator;
+    [SerializeField] public float transitionTime = 1f;
+    [SerializeField] string animatorParamName;
+    [SerializeField] bool hasLevelTransition;
+
+
     public void levelClick()
+    {     
+      StartCoroutine(LoadLevel(levelName));  
+    }
+
+    IEnumerator LoadLevel(string levelName)
     {
+        if (hasLevelTransition)
+        {
+            levelAnimator.SetTrigger("Triggered");
+            yield return new WaitForSeconds(transitionTime);
+        }
         levelSelector.levelToLoad = levelName;
         levelSelector.OpenNewScene();
-    }    
-
+    }
 }
