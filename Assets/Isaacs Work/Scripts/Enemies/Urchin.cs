032f411753f6d234c898e914eb7ef3697902bbc1
IsaacBurbidge <114247599+IsaacBurbidge@users.noreply.github.com>
2024-01-12 12:38:51 +0000

    Urchin Basic Wander/Anim Set Up

--- Assets/Isaacs Work/Scripts/Enemies/Urchin.cs
+++ Assets/Isaacs Work/Scripts/Enemies/Urchin.cs
@@ -7,12 +7,36 @@ public class Urchin : MonoBehaviour
 {
 	[SerializeField]
 	private Vector3 TargetPos;
+	[SerializeField]
+	private Vector3 InitialPos;
+	[SerializeField]
+	private NavMeshAgent Agent;
 
 	private void Start() {
+		Agent = GetComponent<NavMeshAgent>();
+		InitialPos = transform.position;
 		TargetPos = transform.position + new Vector3(0,0,10);
+		Agent.SetDestination(TargetPos);
 	}
 
 	private void Update() {
-		
+		Wander();
 	}
+
+	private void Wander() {
+
+		if (Agent.remainingDistance == 0) {
+			if (transform.position == InitialPos) {
+				Agent.SetDestination(TargetPos);
+			} else {
+				Agent.SetDestination(InitialPos);
+			}
+
+			Debug.Log("hi");
+		}
+		//} else if (transform.position == InitialPos) {
+		//	Agent.SetDestination(TargetPos);
+		//	Debug.Log("yo");
+		//}
+	} 
 }
