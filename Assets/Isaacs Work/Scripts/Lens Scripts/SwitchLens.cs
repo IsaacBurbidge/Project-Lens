37ac732834a06e002d57d53c41fd6fd0c237e4ce
JoshEU <joshjbrown706@gmail.com>
2024-02-16 13:19:08 +0000

    Lens Tint Added

--- Assets/Isaacs Work/Scripts/Lens Scripts/SwitchLens.cs
+++ Assets/Isaacs Work/Scripts/Lens Scripts/SwitchLens.cs
@@ -2,45 +2,75 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class SwitchLens : MonoBehaviour{
+public class SwitchLens : MonoBehaviour {
+	[SerializeField]
+	private Image lensTintImage;
 	public Lens.LensList CurrentLens = Lens.LensList.NONE;
 	Lens[] LensItems;
-	public TextMeshProUGUI CurrentLensText;
+
 	private void Start() {
 		LensItems = (Lens[])FindObjectsOfType(typeof(Lens));
 	}
-
+	private void RedTint() {
+		Color imageColour;
+		imageColour.r = 1.0f;
+		imageColour.g = 0.0f;
+		imageColour.b = 0.0f;
+		imageColour.a = 0.35f;
+		lensTintImage.color = imageColour;
+		lensTintImage.enabled = true;
+		
+	}
+	private void GreenTint() {
+		Color imageColour;
+		imageColour.r = 0.0f;
+		imageColour.g = 1.0f;
+		imageColour.b = 0.0f;
+		imageColour.a = 0.35f;
+		lensTintImage.color = imageColour;
+		lensTintImage.enabled = true;
+	}
+	private void BlueTint() {
+		Color imageColour;
+		imageColour.r = 0.0f;
+		imageColour.g = 0.0f;
+		imageColour.b = 1.0f;
+		imageColour.a = 0.35f;	
+		lensTintImage.color = imageColour;
+		lensTintImage.enabled = true;
+	}
 	public void OnSwapLens() {
 		Lens.LensList PreviousLens = CurrentLens;
-		switch (CurrentLens) { 
+		switch (CurrentLens) {
 			case Lens.LensList.NONE: {
 				CurrentLens = Lens.LensList.REVEAL;
-				CurrentLensText.text = "Reveal Lens";
+				RedTint();
 				break;
 			}
 			case Lens.LensList.REVEAL: {
 				CurrentLens = Lens.LensList.REVERSE;
-				CurrentLensText.text = "Reverse Lens";
+				GreenTint();
 				break;
 			}
 			case Lens.LensList.REVERSE: {
 				CurrentLens = Lens.LensList.REALITY;
-				CurrentLensText.text = "Reality Lens";
+				BlueTint();
 				break;
 			}
 			case Lens.LensList.REALITY: {
 				CurrentLens = Lens.LensList.NONE;
-				CurrentLensText.text = "No Lens";
+				lensTintImage.enabled = false;
 				break;
 			}
 		}
 
-		
-		for (int i = 0; i < LensItems.Length; i++) { 
-			if(CurrentLens == LensItems[i].VisibleLens) {
+		for (int i = 0; i < LensItems.Length; i++) {
+			if (CurrentLens == LensItems[i].VisibleLens) {
 				LensItems[i].ActivateLens();
-			}else if ((int)PreviousLens == (int)LensItems[i].VisibleLens) {
+			}
+			else if ((int)PreviousLens == (int)LensItems[i].VisibleLens) {
 				LensItems[i].DeactivateLens();
 			}
 		}
@@ -50,35 +80,31 @@ public class SwitchLens : MonoBehaviour{
 		Lens.LensList PreviousLens = CurrentLens;
 		CurrentLens = NextLens;
 
-        switch (CurrentLens) {
-            case Lens.LensList.NONE: {
-                CurrentLensText.text = "No Lens";
-				CurrentLensText.color = Color.white;
+		switch (CurrentLens) {
+			case Lens.LensList.NONE: {
+				lensTintImage.enabled = false;
 				break;
-            }
-            case Lens.LensList.REVEAL: {
-                CurrentLensText.text = "Reveal Lens";
-				CurrentLensText.color = Color.red;
+			}
+			case Lens.LensList.REVEAL: {
+				RedTint();
 				break;
-            }
-            case Lens.LensList.REVERSE: {
-                CurrentLensText.text = "Reverse Lens";
-				CurrentLensText.color = Color.green;
+			}
+			case Lens.LensList.REVERSE: {
+				GreenTint();
 				break;
-            }
-            case Lens.LensList.REALITY: {
-                CurrentLensText.text = "Reality Lens";
-				CurrentLensText.color = Color.blue;
+			}
+			case Lens.LensList.REALITY: {
+				BlueTint();
 				break;
-            }
-        }
-
-        for (int i = 0; i < LensItems.Length; i++) {
-            if (CurrentLens == LensItems[i].VisibleLens) {
-                LensItems[i].ActivateLens();
-            } else if ((int)PreviousLens == (int)LensItems[i].VisibleLens) {
-                LensItems[i].DeactivateLens();
-            }
-        }
-    }
+			}
+		}
+		for (int i = 0; i < LensItems.Length; i++) {
+			if (CurrentLens == LensItems[i].VisibleLens) {
+				LensItems[i].ActivateLens();
+			}
+			else if ((int)PreviousLens == (int)LensItems[i].VisibleLens) {
+				LensItems[i].DeactivateLens();
+			}
+		}
+	}
 }
