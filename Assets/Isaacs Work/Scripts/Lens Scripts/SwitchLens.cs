0749ce4522d27c5e778e5b3371709ee5b3f13e18
JoshEU <joshjbrown706@gmail.com>
2024-02-16 13:30:35 +0000

    Lens Wheel in sync with Current lens
    When switching lens via cycling, the Lens Wheel remains in sync visually with the current lens equipped

--- Assets/Isaacs Work/Scripts/Lens Scripts/SwitchLens.cs
+++ Assets/Isaacs Work/Scripts/Lens Scripts/SwitchLens.cs
@@ -7,6 +7,14 @@ using UnityEngine.UI;
 public class SwitchLens : MonoBehaviour {
 	[SerializeField]
 	private Image lensTintImage;
+	[SerializeField]
+	private GameObject revealImage;
+	[SerializeField]
+	private GameObject realityImage;
+	[SerializeField]
+	private GameObject reverseImage;
+	[SerializeField]
+	private GameObject noLenseImage;
 	public Lens.LensList CurrentLens = Lens.LensList.NONE;
 	Lens[] LensItems;
 
@@ -21,7 +29,10 @@ public class SwitchLens : MonoBehaviour {
 		imageColour.a = 0.35f;
 		lensTintImage.color = imageColour;
 		lensTintImage.enabled = true;
-		
+		revealImage.SetActive(true);
+		realityImage.SetActive(false);
+		reverseImage.SetActive(false);
+		noLenseImage.SetActive(false);
 	}
 	private void GreenTint() {
 		Color imageColour;
@@ -31,39 +42,54 @@ public class SwitchLens : MonoBehaviour {
 		imageColour.a = 0.35f;
 		lensTintImage.color = imageColour;
 		lensTintImage.enabled = true;
+		revealImage.SetActive(false);
+		realityImage.SetActive(false);
+		reverseImage.SetActive(true);
+		noLenseImage.SetActive(false);
 	}
 	private void BlueTint() {
 		Color imageColour;
 		imageColour.r = 0.0f;
 		imageColour.g = 0.0f;
 		imageColour.b = 1.0f;
-		imageColour.a = 0.35f;	
+		imageColour.a = 0.35f;
 		lensTintImage.color = imageColour;
 		lensTintImage.enabled = true;
+		revealImage.SetActive(false);
+		realityImage.SetActive(true);
+		reverseImage.SetActive(false);
+		noLenseImage.SetActive(false);
+	}
+	private void NoLensTint() {
+		lensTintImage.enabled = false;
+		revealImage.SetActive(false);
+		realityImage.SetActive(false);
+		reverseImage.SetActive(false);
+		noLenseImage.SetActive(true);
 	}
 	public void OnSwapLens() {
 		Lens.LensList PreviousLens = CurrentLens;
 		switch (CurrentLens) {
 			case Lens.LensList.NONE: {
-				CurrentLens = Lens.LensList.REVEAL;
-				RedTint();
-				break;
-			}
+					CurrentLens = Lens.LensList.REVEAL;
+					RedTint();
+					break;
+				}
 			case Lens.LensList.REVEAL: {
-				CurrentLens = Lens.LensList.REVERSE;
-				GreenTint();
-				break;
-			}
+					CurrentLens = Lens.LensList.REVERSE;
+					GreenTint();
+					break;
+				}
 			case Lens.LensList.REVERSE: {
-				CurrentLens = Lens.LensList.REALITY;
-				BlueTint();
-				break;
-			}
+					CurrentLens = Lens.LensList.REALITY;
+					BlueTint();
+					break;
+				}
 			case Lens.LensList.REALITY: {
-				CurrentLens = Lens.LensList.NONE;
-				lensTintImage.enabled = false;
-				break;
-			}
+					CurrentLens = Lens.LensList.NONE;
+					NoLensTint();
+					break;
+				}
 		}
 
 		for (int i = 0; i < LensItems.Length; i++) {
@@ -82,21 +108,21 @@ public class SwitchLens : MonoBehaviour {
 
 		switch (CurrentLens) {
 			case Lens.LensList.NONE: {
-				lensTintImage.enabled = false;
-				break;
-			}
+					NoLensTint();
+					break;
+				}
 			case Lens.LensList.REVEAL: {
-				RedTint();
-				break;
-			}
+					RedTint();
+					break;
+				}
 			case Lens.LensList.REVERSE: {
-				GreenTint();
-				break;
-			}
+					GreenTint();
+					break;
+				}
 			case Lens.LensList.REALITY: {
-				BlueTint();
-				break;
-			}
+					BlueTint();
+					break;
+				}
 		}
 		for (int i = 0; i < LensItems.Length; i++) {
 			if (CurrentLens == LensItems[i].VisibleLens) {
