87e56442ab5fa8d1f1f0369bad454d0fddf1795f
JoshEU <joshjbrown706@gmail.com>
2024-03-05 12:18:00 +0000

    Small Commit - added max reversible objects to infinite for playtest

--- Assets/Joshs Work/Scripts/TagReversibleObjects.cs
+++ Assets/Joshs Work/Scripts/TagReversibleObjects.cs
@@ -33,7 +33,7 @@ public class TagReversibleObjects : MonoBehaviour {
     public List<GameObject> reversibleObjectsList;
     private int objectToRemoveIndex;
     private RaycastHit hit;
-    private int maxSizeOfList = 100; // change later
+    private int maxSizeOfList = 100; // change laterg
     [SerializeField]
     private SwitchLens swapLensScript;
    
