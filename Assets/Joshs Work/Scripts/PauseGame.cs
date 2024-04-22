fe23d20a7e01e576b6ee9748f21ebe715bfe82f5
JoshEU <joshjbrown706@gmail.com>
2024-04-22 19:45:41 +0100

    Pause Screen Options Menu + VR Left Hand raycast re-position + Menu polishing

--- Assets/Joshs Work/Scripts/PauseGame.cs
+++ Assets/Joshs Work/Scripts/PauseGame.cs
@@ -2,12 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.XR.Interaction.Toolkit.Samples.StarterAssets;
 
 public class PauseGame : MonoBehaviour {
 	[SerializeField]
 	private InputActionProperty pauseGameButton;
 	[SerializeField]
     private GameObject pauseMenuParent;
+	[SerializeField]
+	private DynamicMoveProvider dynamicMoveProvider;
 
     // Update is called once per frame
     void Update() {
@@ -17,5 +20,10 @@ public class PauseGame : MonoBehaviour {
 	}
     private void Pause() {
 		pauseMenuParent.SetActive(true);
+		dynamicMoveProvider.enabled = false;
+	}
+	// Called on Pause Menu Resume Button Press
+	public void UnPause() {
+		dynamicMoveProvider.enabled = true;
 	}
 }
