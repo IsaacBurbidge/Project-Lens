63d3bf238db4405271bb5a31ea646883a54864ac
Finlay John <findominator7@gmail.com>
2024-04-23 15:26:50 +0100

    level 3 puzzle 4 complete (Ending not made yet)
    Level 3 Puzzle 4 is complete! We just need the ending sorted :)

--- Assets/Fins Work/FinsWhiteboxing/sCRIPTS/ItemReset.cs
+++ Assets/Fins Work/FinsWhiteboxing/sCRIPTS/ItemReset.cs
@@ -6,12 +6,15 @@ public class ItemReset : MonoBehaviour
 {
     [SerializeField] private GameObject resetObject;
     [SerializeField] Transform resetTransform;
+    private Rigidbody rb;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject == resetObject)
         {
             resetObject.transform.position = resetTransform.position;
+            rb = resetObject.GetComponent<Rigidbody>();
+            rb.velocity = Vector3.zero;
         }
     }
 }
