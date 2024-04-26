68ad1edddd37d5e383328eb158926e95d32c32f2
JoshEU <joshjbrown706@gmail.com>
2024-04-26 21:57:58 +0100

    Settings Menu functionality in Main Menu finished

--- Assets/Joshs Work/Scripts/SettingsManager.cs
+++ Assets/Joshs Work/Scripts/SettingsManager.cs
@@ -12,6 +12,10 @@ public class SettingsManager : MonoBehaviour {
 	private Slider volumeSlider;
 	[SerializeField]
 	private TextMeshProUGUI volumeValueText;
+	[SerializeField]
+	private TMP_Dropdown lensTintIntensityDropdown;
+	[SerializeField]
+	private Image lensTintImage;
 
 	void Start() {
 		// Add all Audio Sources from the Scene to an Array
@@ -28,6 +32,21 @@ public class SettingsManager : MonoBehaviour {
 		}
 		else {
 			GetVolume();
+		}
+
+		// Checks to see if Tint Intensity has not been previously saved in Player Preferences
+		// If true, then a default value is set to 0.35
+		// If false, then it will retrieve the most recent value in Player preferences
+		if (!PlayerPrefs.HasKey("LensTintIntensity")) {
+			// Sets the default Tint Intensity to 35% if there is no previously saved volume data.
+			PlayerPrefs.SetFloat("LensTintIntensity", 0.35f);
+			lensTintIntensityDropdown.value = (int)0.35f;
+			SetTintIntensity();
+			Debug.Log("NO PREV TINT SET");
+		}
+		else {
+			GetTintIntensity();
+			Debug.Log("GET PREV TINT SET");
 		}
 	}
 
@@ -57,7 +76,39 @@ public class SettingsManager : MonoBehaviour {
 	}
 
 	// Lens Tint Functions:
-	// Slider 1 - 3 
-	// Low Medium High
-	
+	// This function will load the Lens Tint Intensity from Players preferences.
+	public void GetTintIntensity() {
+		lensTintIntensityDropdown.value = (int)PlayerPrefs.GetFloat("LensTintIntensity");
+		SetVolume();
+	}
+	// This function will save the Lens Tint Intensity in Players preferences.
+	public void SetTintIntensity() {
+		PlayerPrefs.SetFloat("LensTintIntensity", lensTintIntensityDropdown.value);
+		UpdateTintIntensity(lensTintIntensityDropdown.value);
+	}
+	// This function will save the Lens Tint Intensity dropdown value to the Player preferences once it has been changed in run-time.
+	public void ChangeTintIntensity() {
+		SetTintIntensity();
+		UpdateTintIntensity(lensTintIntensityDropdown.value);
+	}
+	public void UpdateTintIntensity(float intensity) {
+		Color lensTintColour = lensTintImage.color;
+		switch (intensity) {
+			case 0:
+				// Low Intensity
+				lensTintColour.a = 0.25f;
+				lensTintImage.color = lensTintColour;
+				break;
+			case 1:
+				// Medium Intensity
+				lensTintColour.a = 0.35f;
+				lensTintImage.color = lensTintColour;
+				break;
+			case 2:
+				// High Intensity
+				lensTintColour.a = 0.45f;
+				lensTintImage.color = lensTintColour;
+				break;
+		}
+	}
 }
